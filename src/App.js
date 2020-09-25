205b7279dc148b98abd4f90b742ef2e1c7243ae7
jack <66040370+Jnpalermo@users.noreply.github.com>
2020-09-25 14:32:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as fireData from "./data/fireData.json";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.1258,
+    longitude: -119.3193,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
+  const [selectedFire, setSelectedFire] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoieXVuZ2JhZSIsImEiOiJja2YwNGxibGcxZWt0MnlwOXhqazBweTB1In0.Krl5dpCJouPQuG-1d8FwvA"
+        mapStyle="mapbox://styles/yungbae/ckf05h4i82fny1bo6qt37xd2w"
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        {fireData.data.fires.map((fire) => (
+          <Marker
+            key={fire.confidence}
+            latitude={fire.position.lat}
+            longitude={fire.position.lon}
+          >
+            <button
+              className="marker-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                debugger;
+                setSelectedFire(fire);
+              }}
+            >
+              <img src="/flame.svg" alt="flame icon "></img>
+            </button>
+            <div>FIRE</div>
+          </Marker>
+        ))}
+        {selectedFire ? (
+          <Popup
+            latitude={selectedFire.position.lat}
+            longitude={selectedFire.position.lon}
+            onClose={() => {
+              debugger;
+              setSelectedFire(null);
+            }}
+          >
+            <div>
+              <h2>{selectedFire.details.status}</h2>
+            </div>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
     </div>
   );
 }
-
-export default App;
